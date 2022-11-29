710c5dd030228c4fef3fe4a1e99a74d3e7a712d2
masyuks <myukimiftakhurrizqi21@gmail.com>
2022-11-29 08:40:43 +0700

    update

--- AccessModifier/src/com/dicoding/javafundamental/accessmodifier/package2/KelasB.java
+++ AccessModifier/src/com/dicoding/javafundamental/accessmodifier/package2/KelasB.java
@@ -1,2 +1,12 @@
-package com.dicoding.javafundamental.accessmodifier.package2;public class KelasB {
+package com.dicoding.javafundamental.accessmodifier.package2;
+
+import com.dicoding.javafundamental.accessmodifier.package1.KelasA;
+
+public class KelasB extends KelasA {
+
+    @Override
+    protected void methodC() {
+        super.methodC();
+        System.out.println("Contoh pemanggilan protected dari package luar");
+    }
 }
--- AccessModifier/src/com/dicoding/javafundamental/accessmodifier/package3/Perhitungan.java
+++ AccessModifier/src/com/dicoding/javafundamental/accessmodifier/package3/Perhitungan.java
@@ -1,2 +1,13 @@
-package com.dicoding.javafundamental.accessmodifier.package3;public class Perhitungan {
+package com.dicoding.javafundamental.accessmodifier.package3;
+
+public class Perhitungan {
+    public static int nilai = 0;
+
+    protected static int getNilai(){
+        return nilai;
+    }
+
+    Perhitungan() {
+        nilai++;
+    }
 }
